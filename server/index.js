cb3a206a7e4052cba1b713202a006ddb07f3e518
Dharmapra2 <dharmapra865@gmail.com>
2023-04-01 16:00:08 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
+import graphqlHttp from "express-graphql";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import cors from "cors";
 import connectMongodb from "./Config/connection.js";
 import dotenv from "dotenv";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import UserRoutes from "./Routes/UserRoutes.js";
 import PostRoutes from "./Routes/PostRoutes.js";
+
+import * as graphqlSchema from "./Graphql/Resolvers/index.js";
 
 /* declearing port */
 dotenv.config();
@@ -18,6 +20,15 @@ mongoose.set("strictQuery", true);
 /* bodyParser.json() or express.json() is used for add the built-in JSON body parser to properly add the "body" property to the request object. */
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true,
+  })
+);
 
 /* connecting to mongodb */
 await connectMongodb();
