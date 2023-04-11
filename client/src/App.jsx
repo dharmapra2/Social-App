85a406a5eab78cf3d293caeafd3047835edd34fd
Dharmapra2 <dharmapra865@gmail.com>
2023-04-11 23:46:05 +0530

    update desing and modify logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
     <div className="App">
       <div className="blur -top-[18%] right-0"></div>
       <div className="blur top-[36%] -left-[8rem]"></div>
-      <Home />
+      {/* <Home /> */}
       {/* <Profile /> */}
-      {/* <Auth /> */}
+      <Auth />
     </div>
   );
 }
--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
 function Auth() {
+  const initialData = {
+    userName: "",
+    first_name: "",
+    last_name: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+  };
+  const [isSignedUp, setIsSignedUp] = useState(true);
+  const [formInput, setformInput] = useState(initialData);
+  const handleInputChange = (event) => {
+    setformInput({ ...formInput, [event.target.name]: event?.target?.value });
+  };
+  const handleSubmit = (event) => {
+    event?.preventDefault();
+    console.log(formInput);
+  };
   return (
     <div className="Auth">
+      {/* left part */}
       <div className="a-left">
         <img src={Logo} alt="Logo" />
         <div className="webName">
@@ -11,90 +29,124 @@ function Auth() {
           <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
-      <Login />
-    </div>
-  );
-}
-function SignUp() {
-  return (
-    <div className="a-right">
-      <form className="infoFrom authForm">
-        <h3>Sign up</h3>
-        <div className="">
-          <input
-            type="text"
-            placeholder="First Name"
-            className="infoInput"
-            name="firstName"
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="infoInput"
-            name="lastName"
-          />
+      {/* right part */}
+      {isSignedUp ? (
+        <div className="a-right">
+          <form className="infoFrom authForm" onSubmit={handleSubmit}>
+            <h3>Sign up</h3>
+            <div className="">
+              <input
+                type="text"
+                placeholder="First Name"
+                className="infoInput"
+                defaultValue={formInput?.first_name}
+                name="first_name"
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                className="infoInput"
+                defaultValue={formInput?.last_name}
+                name="last_name"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <input
+                type="text"
+                placeholder="User Name"
+                className="infoInput"
+                defaultValue={formInput?.userName}
+                name="userName"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <input
+                type="email"
+                placeholder="Email"
+                className="infoInput"
+                defaultValue={formInput?.email}
+                name="email"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <input
+                type="password"
+                placeholder="Password"
+                className="infoInput"
+                defaultValue={formInput?.password}
+                name="password"
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                className="infoInput"
+                defaultValue={formInput?.confirmPassword}
+                name="confirmPassword"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <span
+                className="text-[12px] cursor-pointer"
+                onClick={() => {
+                  setIsSignedUp(!isSignedUp);
+                  setformInput((prev) => initialData);
+                }}
+              >
+                Already have an account. Login!
+              </span>
+            </div>
+            <button className="button infoBtn" type="submit">
+              Signup
+            </button>
+          </form>
         </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="User Name"
-            className="infoInput"
-            name="userName"
-          />
+      ) : (
+        <div className="a-right">
+          <form className="infoFrom authForm" onSubmit={handleSubmit}>
+            <h3>Log In</h3>
+            <div className="">
+              <input
+                type="text"
+                placeholder="User Name"
+                className="infoInput"
+                name="userName"
+                defaultValue={formInput?.userName}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <input
+                type="password"
+                placeholder="Password"
+                className="infoInput"
+                name="password"
+                defaultValue={formInput?.password}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="">
+              <span
+                className="text-[12px] cursor-pointer"
+                onClick={() => {
+                  setIsSignedUp(!isSignedUp);
+                  setformInput((prev) => initialData);
+                }}
+              >
+                Don't have an account. SignUp!
+              </span>
+              <button className="button infoBtn" type="submit">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="Password"
-            className="infoInput"
-            name="password"
-          />
-          <input
-            type="text"
-            placeholder="Confirm Password"
-            className="infoInput"
-            name="confirmPassword"
-          />
-        </div>
-        <div className="">
-          <span className="text-[12px]">Already have an account. Login!</span>
-        </div>
-        <button className="button infoBtn" type="submit">
-          Signup
-        </button>
-      </form>
-    </div>
-  );
-}
-
-function Login() {
-  return (
-    <div className="a-right">
-      <form className="infoFrom authForm">
-        <h3>Log In</h3>
-        <div className="">
-          <input
-            type="text"
-            placeholder="User Name"
-            className="infoInput"
-            name="userName"
-          />
-        </div>
-        <div className="">
-          <input
-            type="text"
-            placeholder="Password"
-            className="infoInput"
-            name="password"
-          />
-        </div>
-        <div className="">
-          <span className="text-[12px]">Don't have an account. SignUp!</span>
-          <button className="button infoBtn" type="submit">
-            Login
-          </button>
-        </div>
-      </form>
+      )}
     </div>
   );
 }
