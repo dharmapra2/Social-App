3879ec2145e7f608ead4c5a9eecad38f55bfd78e
Dharmapra2 <dharmapra865@gmail.com>
2023-05-06 10:57:15 +0530

    write readme.md file   and dockerized the project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# social-app
-## hello
+###Social App - GraphQL Version
+The #social-app-graphql branch of the Social App project is an alternate version of the application that uses GraphQL as its API layer. This branch is designed to showcase the benefits of using GraphQL over a traditional REST API.
+
+##Technologies
+The server is built using Node.js and Express.js with the Apollo Server library for GraphQL implementation. The client-side of the application is built with React.js and Apollo Client library for GraphQL.
+
+#Benefits of GraphQL
+By using GraphQL, the application benefits from reduced network requests and over-fetching of data, as the client can query exactly what it needs from the server. The GraphQL schema also provides a clear and comprehensive description of the available data and operations.
+
+#Conclusion
+Overall, the social-app-graphql branch provides an example of how GraphQL can be used to create a more efficient and flexible API layer for modern web applications.
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-import graphqlHttp from "express-graphql";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
 import mongoose from "mongoose";
 import connectMongodb from "./Config/connection.js";
 import dotenv from "dotenv";
@@ -8,27 +9,21 @@ import AuthRoutes from "./Routes/AuthRoutes.js";
 import UserRoutes from "./Routes/UserRoutes.js";
 import PostRoutes from "./Routes/PostRoutes.js";
 
-import * as graphqlSchema from "./Graphql/Resolvers/index.js";
-
 /* declearing port */
 dotenv.config();
 const PORT = process.env.HOST_PORT || 4000;
 const app = express();
 
 mongoose.set("strictQuery", true);
-
+// Allow requests from http://localhost:3000
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 /* bodyParser.json() or express.json() is used for add the built-in JSON body parser to properly add the "body" property to the request object. */
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-
-app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: graphqlSchema,
-    rootValue: graphqlResolvers,
-    graphiql: true,
-  })
-);
 
 /* connecting to mongodb */
 await connectMongodb();
@@ -37,6 +32,10 @@ await connectMongodb();
 app.use("/auth", AuthRoutes);
 app.use("/user", UserRoutes);
 app.use("/post", PostRoutes);
+// Route handler for GET /
+app.get("/hi", (req, res) => {
+  res.sendFile(path.join(path.resolve(), 'index.html'));
+});
 
 // Error handler middleware
 app.use((err, req, res, next) => {
